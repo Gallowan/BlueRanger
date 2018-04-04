4516bdaff887a2d700b43c36b96891f528c6ebc2
ieabbas <the0nlyismail@gmail.com>
2018-04-04 00:53:03 -0700

    Created News Feed, News Feed Content, and Sports Selection Activites, need to link login to news feed to link to Sports Selection

---
+++ ScoutHQ1/app/src/main/java/com/example/gallo/scouthq1/SportsSelection.java
@@ -0,0 +1,13 @@
+package com.example.gallo.scouthq1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SportsSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sports_selection);
+    }
+}
