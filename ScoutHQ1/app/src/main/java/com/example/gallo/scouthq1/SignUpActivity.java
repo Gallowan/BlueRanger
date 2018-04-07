a4997963fbb66d969a41f343b04e79528d54eb69
mkhuluf <rmicheal48@gmail.com>
2018-04-07 07:50:14 +0100

    I got firebase to register users in SignUpActivity

--- ScoutHQ1/app/src/main/java/com/example/gallo/scouthq1/SignUpActivity.java
+++ ScoutHQ1/app/src/main/java/com/example/gallo/scouthq1/SignUpActivity.java
@@ -8,12 +8,80 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    EditText editTextEmail, editTextPassword, getEditTextPasswordReenter;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextEmail = (EditText) findViewById(R.id.field_email_address);
+        editTextPassword = (EditText) findViewById(R.id.field_password);
+        getEditTextPasswordReenter = (EditText) findViewById(R.id.field_password_reenter);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.join_btn).setOnClickListener(this);
+    }
+
+    private void registerUser()
+    {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String passwordReenter = getEditTextPasswordReenter.getText().toString().trim();
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            editTextEmail.setError("Please enter a valid email address");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(email.isEmpty())
+        {
+            editTextEmail.setError("Email is required.");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty())
+        {
+            editTextPassword.setError("Password is required.");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(!password.equals(passwordReenter))
+        {
+            getEditTextPasswordReenter.setError("Password doesn't match");
+            getEditTextPasswordReenter.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6)
+        {
+            editTextPassword.setError("Not enough characters for password");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(),"User registration successful!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -21,7 +89,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         switch(view.getId())
         {
             case R.id.join_btn:
-//                registerUser();
+                registerUser();
 
                 break;
         }
